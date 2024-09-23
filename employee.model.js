3d1f18368a310c7173c3a8436c080bd51bf8e100
gopika <gopikaksurendran@gmail.com>
2024-09-23 11:02:45 +0530

    add

---
+++ employee.model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+
+
+const empSchema=new mongoose.Schema({
+    ID:{type:String},
+    name:{type:String},
+    salary:{type:Number},
+    designation:{type:String},
+    experience:{type:Number},
+    phone:{type:String},
+    email:{type:String}
+
+})
+
+export default mongoose.model.employees||mongoose.model("employee",empSchema)
